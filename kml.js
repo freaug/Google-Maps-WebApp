9efb7367cf44621408f16fbf332dac8f78bc6613
freaug <efarr21188@gmail.com>
2016-08-01 23:28:55 -0400

    Some updates

--- kml.js
+++ kml.js
@@ -1,15 +1,16 @@
 var map;
-var center;
 var layers = [];
 
 function initMap() {
 	var myLatLng = new google.maps.LatLng(33.760654,-84.355205);
 	var myOptions = {
 		zoom: 11,
-		center: myLatLng	
+		center: myLatLng,
+		mapTypeControl: false,
+		streetViewControl: false	
 	}
  	map = new google.maps.Map(document.getElementById('map'), myOptions);
-
+ 	//fill the array
 	layers [0] = new google.maps.KmlLayer('https://sites.google.com/site/eddieskmlfiles/kml-files/Shuttle%20Route.kmz?attredirects=0&d=1', {preserveViewport: false, suppressInfoWindows: false}); 
 	layers [1] = new google.maps.KmlLayer('https://sites.google.com/site/eddieskmlfiles/kml-files/Parade%20Route.kmz?attredirects=0&d=1', {preserveViewport: true, suppressInfoWindows: false});
 	layers [2] = new google.maps.KmlLayer('https://sites.google.com/site/eddieskmlfiles/kml-files/Art%20Vendors.kmz?attredirects=0&d=1', {preserveViewport: true, suppressInfoWIndows: false});
@@ -19,15 +20,11 @@ function initMap() {
 	layers [6] = new google.maps.KmlLayer('https://sites.google.com/site/eddieskmlfiles/kml-files/Stages.kmz?attredirects=0&d=1',{preserveViewport: true, suppressInfoWindows: false});
 	layers [7] = new google.maps.KmlLayer('https://sites.google.com/site/eddieskmlfiles/kml-files/Street%20Market%20Booth%20Numbers.kmz?attredirects=0&d=1',{preserveViewport: true, suppressInfoWindows: false});
 	layers [8] = new google.maps.KmlLayer('https://sites.google.com/site/eddieskmlfiles/kml-files/Tour%20of%20Homes.kmz?attredirects=0&d=1',{preserveViewport: true, suppressInfoWindows: false});
+	//initialize the map to the festival area
+	layers[0].setMap(map);
+	//set other layers to null
 	for(var i = 1; i < layers.length; i++){
 		layers[i].setMap(null);
-	
-function calculateCenter() {
-	center = map.getCenter();
-		google.maps.event.addDomListener(map, 'idle', function() {calculateCenter();});
-		google.maps.event.addDomListener(window, 'resize', function() { map.setCenter(center);});
-	}
-
 	}
 }
 
